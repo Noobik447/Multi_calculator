07a6d92ea4a046779c8c1bd66731ba1a9ad1ae00
Noobik447 <noobik477@gmail.com>
2023-11-19 22:34:31 +0700

    добавлен перевод данных

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ class MyWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.settings = QtCore.QSettings("Настройки", "Мульти_калькулятор")
+
+        self.memory_message = 0
+        self.memory = ""
         
         if self.settings.contains("Окно/Местоположение"):
             self.setGeometry(self.settings.value("Окно/Местоположение"))
         else:
             self.resize(200, 100)
-            
         if self.settings.contains("Режимы/Режим"):
             if self.settings.value("Режимы/Режим") == "calc":
                 self.calc()
@@ -19,14 +21,19 @@ class MyWindow(QtWidgets.QMainWindow):
                 self.code()
             elif self.settings.value("Режимы/Режим") == "temp":
                 self.temp()
+      
             elif self.settings.value("Режимы/Режим") == "length":
                 self.length()
             elif self.settings.value("Режимы/Режим") == "weight":
                 self.weight()
             elif self.settings.value("Режимы/Режим") == "time":
                 self.time()
+            elif self.settings.value("Режимы/Режим") == "data":
+                self.data()
         else:
             self.calc()
+        if self.settings.contains("Память/Память"):
+            self.memory_message = self.settings.value("Память/Память")
         
         self._createActions()
         self._createMenuBar()
@@ -34,13 +41,22 @@ class MyWindow(QtWidgets.QMainWindow):
     def _createMenuBar(self):
         menuBar = self.menuBar()
         calcMenu = QtWidgets.QMenu("Режим", self)
+        memoryMenu = QtWidgets.QMenu("Память", self)
         menuBar.addMenu(calcMenu)
+        menuBar.addMenu(memoryMenu)
         calcMenu.addAction(self.calcAction)
         calcMenu.addAction(self.codeAction)
         calcMenu.addAction(self.tempAction)
         calcMenu.addAction(self.lengthAction)
         calcMenu.addAction(self.weightAction)
         calcMenu.addAction(self.timeAction)
+        calcMenu.addAction(self.dataAction)
+        memoryMenu.addAction(self.memory_saveAction)
+        memoryMenu.addAction(self.memory_readAction)
+        memoryMenu.addAction(self.memory_clearAction)
+        memoryMenu.addAction(self.memory_addAction)
+        memoryMenu.addAction(self.memory_subAction)
+        menuBar.addAction(self.copyAction)
         
     def _createActions(self):
         self.calcAction = QtWidgets.QAction("Калькулятор", self)
@@ -55,7 +71,21 @@ class MyWindow(QtWidgets.QMainWindow):
         self.weightAction.triggered.connect(self.weight)
         self.timeAction = QtWidgets.QAction("Время", self)
         self.timeAction.triggered.connect(self.time)
-        
+        self.dataAction = QtWidgets.QAction("Данные", self)
+        self.dataAction.triggered.connect(self.data)
+        self.memory_addAction = QtWidgets.QAction("M+", self)
+        self.memory_subAction = QtWidgets.QAction("M-", self)
+        self.memory_saveAction = QtWidgets.QAction("MS", self)
+        self.memory_readAction = QtWidgets.QAction("MR", self)
+        self.memory_clearAction = QtWidgets.QAction("MC", self)
+        self.memory_saveAction.triggered.connect(self.memory_save)
+        self.memory_readAction.triggered.connect(self.memory_read)
+        self.memory_clearAction.triggered.connect(self.memory_clear)
+        self.memory_addAction.triggered.connect(self.memory_add)
+        self.memory_subAction.triggered.connect(self.memory_sub)
+        self.copyAction = QtWidgets.QAction("Скопировать результат", self)
+        self.copyAction.triggered.connect(self.copy_result)
+
     def closeEvent(self, event):
         self.settings.beginGroup("Окно")
         self.settings.setValue("Местоположение", self.geometry())
@@ -63,6 +93,10 @@ class MyWindow(QtWidgets.QMainWindow):
         
         self.settings.beginGroup("Режимы")
         self.settings.setValue("Режим", self.mode)
+        self.settings.endGroup()
+
+        self.settings.beginGroup("Память")
+        self.settings.setValue("Память", self.memory_message)
         self.settings.endGroup()
 
     
@@ -114,7 +148,7 @@ class MyWindow(QtWidgets.QMainWindow):
         
         self.mode = "calc"
     
-    #Перевод в системы счисления из десятичной
+    #Перевод в системы счисления
     def code(self):
         self.setWindowTitle("Перевод в системы счисления")
         self.vbox = QtWidgets.QVBoxLayout()
@@ -383,6 +417,67 @@ class MyWindow(QtWidgets.QMainWindow):
         wid.setLayout(self.vbox)
 
         self.mode = "time"
+
+    def data(self):
+        self.setWindowTitle("Данные")
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.grid = QtWidgets.QGridLayout()
+        wid = QtWidgets.QWidget(self)
+        self.setCentralWidget(wid)
+
+        self.lb = QtWidgets.QLabel("0")
+        self.le = QtWidgets.QLineEdit()
+        self.btn = QtWidgets.QPushButton("Перевести")
+        self.btn.clicked.connect(self.translate_data)
+        self.btn.setShortcut(QtGui.QKeySequence("Return"))
+
+        self.rb = QtWidgets.QRadioButton("Из бит")
+        self.rb2 = QtWidgets.QRadioButton("Из байт")
+        self.rb3 = QtWidgets.QRadioButton("Из килобайт")
+        self.rb4 = QtWidgets.QRadioButton("Из мегабайт")
+        self.rb5 = QtWidgets.QRadioButton("Из гигабайт")
+        self.rb6 = QtWidgets.QRadioButton("Из терабайт")
+
+        self.button_group = QtWidgets.QButtonGroup()
+        self.button_group.addButton(self.rb)
+        self.button_group.addButton(self.rb2)
+        self.button_group.addButton(self.rb3)
+        self.button_group.addButton(self.rb4)
+        self.button_group.addButton(self.rb5)
+        self.button_group.addButton(self.rb6)
+        
+        self.rbb = QtWidgets.QRadioButton("В биты")
+        self.rbb2 = QtWidgets.QRadioButton("В байты")
+        self.rbb3 = QtWidgets.QRadioButton("В килобайты")
+        self.rbb4 = QtWidgets.QRadioButton("В мегабайты")
+        self.rbb5 = QtWidgets.QRadioButton("В гигабайты")
+        self.rbb6 = QtWidgets.QRadioButton("В терабайты")
+
+        self.vbox.addWidget(self.lb)
+        self.vbox.addWidget(self.le)
+
+        self.grid.addWidget(self.rb, 0, 0)
+        self.grid.addWidget(self.rb2, 1, 0)
+        self.grid.addWidget(self.rb3, 2, 0)
+        self.grid.addWidget(self.rb4, 3, 0)
+        self.grid.addWidget(self.rb5, 4, 0)
+        self.grid.addWidget(self.rb6, 5, 0)
+
+        self.grid.addWidget(self.rbb, 0, 1)
+        self.grid.addWidget(self.rbb2, 1, 1)
+        self.grid.addWidget(self.rbb3, 2, 1)
+        self.grid.addWidget(self.rbb4, 3, 1)
+        self.grid.addWidget(self.rbb5, 4, 1)
+        self.grid.addWidget(self.rbb6, 5, 1)
+
+        self.vbox.addWidget(self.lb)
+        self.vbox.addWidget(self.le)
+
+        self.vbox.addLayout(self.grid)
+        self.vbox.addWidget(self.btn)
+        wid.setLayout(self.vbox)
+
+        self.mode = "data"
 
     def add(self):
         res = float(self.le1.text()) + float(self.le2.text())
@@ -898,6 +993,239 @@ class MyWindow(QtWidgets.QMainWindow):
 
         self.lb.setText(str(converted_time))
 
+    def translate_data(self):
+        #Биты
+        if self.rb.isChecked():
+            inn = 1
+        #Байты
+        elif self.rb2.isChecked():
+            inn = 2
+        #Килобайты
+        elif self.rb3.isChecked():
+            inn = 3
+        #Мегабйты
+        elif self.rb4.isChecked():
+            inn = 4
+        #Гигабайты
+        elif self.rb5.isChecked():
+            inn = 5
+        #Терабайты
+        elif self.rb6.isChecked():
+            inn = 6
+
+
+        #Биты
+        if self.rbb.isChecked():
+            out = 1
+        #Байты
+        elif self.rbb2.isChecked():
+            out = 2
+        #Килобайты
+        elif self.rbb3.isChecked():
+            out = 3
+        #Мегабйты
+        elif self.rbb4.isChecked():
+            out = 4
+        #Гигабайты
+        elif self.rbb5.isChecked():
+            out = 5
+        #Терабайты
+        elif self.rbb6.isChecked():
+            out = 6
+
+        if inn == 1:
+            if out == 2:
+                converted_time = self.bits_to_bytes(self.le.text())
+            elif out == 3:
+                converted_time = self.bits_to_kilobytes(self.le.text())
+            elif out == 4:
+                converted_time = self.bits_to_megabytes(self.le.text())
+            elif out == 5:
+                converted_time = self.bits_to_gigabytes(self.le.text())
+            elif out == 6:
+                converted_time = self.bits_to_terabytes(self.le.text())
+        elif inn == 2:
+            if out == 1:
+                converted_time = self.bytes_to_bits(self.le.text())
+            elif out == 3:
+                converted_time = self.bytes_to_kilobytes(self.le.text())
+            elif out == 4:
+                converted_time = self.bytes_to_megabytes(self.le.text())
+            elif out == 5:
+                converted_time = self.bytes_to_gigabytes(self.le.text())
+            elif out == 6:
+                converted_time = self.bytes_to_terabytes(self.le.text())
+        elif inn == 3:
+            if out == 1:
+                converted_time = self.kilobytes_to_bits(self.le.text())
+            elif out == 2:
+                converted_time = self.kilobytes_to_bytes(self.le.text())
+            elif out == 4:
+                converted_time = self.kilobytes_to_megabytes(self.le.text())
+            elif out == 5:
+                converted_time = self.kilobytes_to_gigabytes(self.le.text())
+            elif out == 6:
+                converted_time = self.kilobytes_to_terabytes(self.le.text())
+        elif inn == 4:
+            if out == 1:
+                converted_time = self.megabytes_to_bits(self.le.text())
+            elif out == 2:
+                converted_time = self.megabytes_to_bytes(self.le.text())
+            elif out == 3:
+                converted_time = self.megabytes_to_kilobytes(self.le.text())
+            elif out == 5:
+                converted_time = self.megabytes_to_gigabytes(self.le.text())
+            elif out == 6:
+                converted_time = self.megabytes_to_terabytes(self.le.text())
+        elif inn == 5:
+            if out == 1:
+                converted_time = self.gigabytes_to_bits(self.le.text())
+            elif out == 2:
+                converted_time = self.gigabytes_to_bytes(self.le.text())
+            elif out == 3:
+                converted_time = self.gigabytes_to_kilobytes(self.le.text())
+            elif out == 4:
+                converted_time = self.gigabytes_to_megabytes(self.le.text())
+            elif out == 6:
+                converted_time = self.gigabytes_to_terabytes(self.le.text())
+        elif inn == 6:
+            if out == 1:
+                converted_time = self.terabytes_to_bits(self.le.text())
+            elif out == 2:
+                converted_time = self.terabytes_to_bytes(self.le.text())
+            elif out == 3:
+                converted_time = self.terabytes_to_kilobytes(self.le.text())
+            elif out == 4:
+                converted_time = self.terabytes_to_megabytes(self.le.text())
+            elif out == 5:
+                converted_time = self.terabytes_to_gigabytes(self.le.text())
+
+        if inn == out:
+            converted_time = self.le.text()
+
+        self.lb.setText(str(converted_time))
+
+
+    def bits_to_bytes(self, bits):
+        bytes = float(bits) / 8
+        return bytes
+
+    def bits_to_kilobytes(self, bits):
+        kilobytes = float(bits) / 8192
+        return kilobytes
+
+    def bits_to_megabytes(self, bits):
+        megabytes = float(bits) / 8388608
+        return megabytes
+    
+    def bits_to_gigabytes(self, bits):
+        gigabytes = float(bits) / 8589934592
+        return gigabytes
+    
+    def bits_to_terabytes(self, bits):
+        terabytes = float(bits) / 8796093022208
+        return terabytes
+    
+    def bytes_to_bits(self, bytes):
+        bits = float(bytes) * 8
+        return bits
+
+    def bytes_to_kilobytes(self, bytes):
+        kilobytes = float(bytes) / 1024
+        return kilobytes
+
+    def bytes_to_megabytes(self, bytes):
+        megabytes = float(bytes) / 1048576
+        return megabytes
+    
+    def bytes_to_gigabytes(self, bytes):
+        gigabytes = float(bytes) / 8589934592
+        return gigabytes
+
+    def bytes_to_terabytes(self, bytes):
+        terabytes = float(bytes) / 8796093022208
+        return terabytes
+
+    def kilobytes_to_bits(self, kilobytes):
+        bits = float(kilobytes) * 8192
+        return bits
+
+    def kilobytes_to_bytes(self, kilobytes):
+        bytes = float(kilobytes) * 1024
+        return bytes
+
+    def kilobytes_to_megabytes(self, kilobytes):
+        megabytes = float(kilobytes) / 1024
+        return megabytes
+
+    def kilobytes_to_gigabytes(self, kilobytes):
+        gigabytes = float(kilobytes) / 1048576
+        return gigabytes
+
+    def kilobytes_to_terabytes(self, kilobytes):
+        terabytes = float(kilobytes) / 1073741824
+        return terabytes
+    
+    def megabytes_to_bits(self, megabytes):
+        bits = float(megabytes) * 8388608
+        return bits
+
+    def megabytes_to_bytes(self, megabytes):
+        bytes = float(megabytes) * 1048576
+        return bytes
+
+    def megabytes_to_kilobytes(self, megabytes):
+        kilobytes = float(megabytes) * 1024
+        return kilobytes
+
+    def megabytes_to_gigabytes(self, megabytes):
+        gigabytes = float(megabytes) / 1024
+        return gigabytes
+
+    def megabytes_to_terabytes(self, megabytes):
+        terabytes = float(megabytes) / 1048576
+        return terabytes
+
+    def gigabytes_to_bits(self, gigabytes):
+        bits = float(gigabytes) * 8589934592
+        return bits
+
+    def gigabytes_to_bytes(self, gigabytes):
+        bytes = float(gigabytes) * 1073741824
+        return bytes
+
+    def gigabytes_to_kilobytes(self, gigabytes):
+        kilobytes = float(gigabytes) * 1048576
+        return kilobytes
+
+    def gigabytes_to_megabytes(self, gigabytes):
+        megabytes = float(gigabytes) * 1024
+        return megabytes
+
+    def gigabytes_to_terabytes(self, gigabytes):
+        terabytes = float(gigabytes) / 1024
+        return terabytes
+    
+    def terabytes_to_bits(self, terabytes):
+        bits = float(terabytes) * 8796093022208
+        return bits
+
+    def terabytes_to_bytes(self, terabytes):
+        bytes = float(terabytes) * 1099511627776
+        return bytes
+
+    def terabytes_to_kilobytes(self, terabytes):
+        kilobytes = float(terabytes) * 1073741824
+        return kilobytes
+
+    def terabytes_to_megabytes(self, terabytes):
+        megabytes = float(terabytes) * 1048576
+        return megabytes
+
+    def terabytes_to_gigabytes(self, terabytes):
+        gigabytes = float(terabytes) * 1024
+        return gigabytes
+
     def milliseconds_to_seconds(self, milliseconds):
         seconds = float(milliseconds) / 1000
         return seconds
@@ -1121,7 +1449,53 @@ class MyWindow(QtWidgets.QMainWindow):
     def years_to_months(self, years):
         months = float(years) * 12
         return months
+    
+    def memory_save(self):
+        try:
+            self.memory = self.le.text()
+        except AttributeError:
+            self.memory = self.le1.text()
+        if self.memory_message == 0:
+            QtWidgets.QMessageBox.about(self, "Память", "В память сохраняется 1 строка")
+            self.memory_message = 1
+            
 
+    def memory_read(self):
+        if self.mode == "calc":
+            memory_read_message = QtWidgets.QMessageBox(self, "MR", "В какую строку вставить. Да - в первую, нет в вторую?", QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+            if memory_read_message == QtWidgets.QMessageBox.Yes:
+                try:
+                    self.le.setText(self.memory)
+                except AttributeError:
+                    self.le1.setText(self.memory)
+            elif memory_read_message == QtWidgets.QMessageBox.No:
+                self.le2.setText(self.memory)
+        else:
+            self.le.setText(self.memory)
+
+    def memory_clear(self):
+        self.memory = ""
+
+    def memory_add(self):
+        num1 = self.lb.text()
+        if self.memory != "":
+            res = float(num1) + int(self.memory)
+            self.lb.setText(str(res))
+        else:
+            self.setWindowTitle("Память пустая")
+        
+
+    def memory_sub(self):
+        num1 = self.lb.text()
+        if self.memory != "":
+            res = float(num1) - int(self.memory)
+            self.lb.setText(str(res))
+        else:
+            self.setWindowTitle("Память пустая")
+
+    def copy_result(self):
+        clipboard = QtWidgets.QApplication.clipboard()
+        clipboard.setText(self.lb.text())
 
 if __name__ == "__main__":
     import sys
